879c0004be7e823f28bb18283ce883e5ba24c0c1
scheng0 <scheng2019@gmail.com>
2019-10-19 23:20:24 -0400

    a

--- project/main.py
+++ project/main.py
@@ -1,6 +1,6 @@
 # main.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from .models import Event
 from . import db
