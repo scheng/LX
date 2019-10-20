d4c0e28c27f7c86a53b58885f11bca8cacd32bae
scheng0 <scheng2019@gmail.com>
2019-10-20 11:16:27 -0400

    timer checker

--- checker.py
+++ checker.py
@@ -12,6 +12,7 @@ while 1:
     h = time.time()%(24*3600)/3600-4 #the hour
 #    print ([(User.query.get(e.user_id).email, e.stop) for e in Event.query.all()])
 #    a = ([e.user_id for e in Event.query.filter(Event.time - 0 < 10).all()])
+    print ("checking ", h)
     for e in Event.query.filter(Event.time - h < 0.01, Event.time-h > -0.01).all(): #date stuff here
         print (User.query.get(e.user_id).email, e.stop, e.route) #phone number, and two bus stops
         main(e.route, e.stop, phone_number=User.query.get(e.user_id).email)
