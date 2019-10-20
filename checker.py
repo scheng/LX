f18da210b8aad63769321afecc5cfe70d63531d0
scheng0 <scheng2019@gmail.com>
2019-10-20 08:47:58 -0400

    finally work?

--- checker.py
+++ checker.py
@@ -10,8 +10,8 @@ app.app_context().push()
 
 while 1:
 #    print ([(User.query.get(e.user_id).email, e.stop) for e in Event.query.all()])
-    a = ([e.user_id for e in Event.query.filter(Event.time - 0 < 10).all()])
-    for e in Event.query.filter(Event.time-0 < 0.1).all(): #date stuff here
+#    a = ([e.user_id for e in Event.query.filter(Event.time - 0 < 10).all()])
+    for e in Event.query.filter(Event.time-0 < 10).all(): #date stuff here
         print (User.query.get(e.user_id).email, e.stop, e.route) #phone number, and two bus stops
         main(e.route, e.stop, phone_number=User.query.get(e.user_id).email)
     time.sleep(60)
