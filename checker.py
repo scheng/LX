e3cd1505b2dd5f7de3468ac38a117c025d483d84
scheng0 <scheng2019@gmail.com>
2019-10-20 05:35:15 -0400

    fuck git

--- checker.py
+++ checker.py
@@ -3,11 +3,14 @@ import time
 from project import create_app
 from project.models import Event
 from project.models import User
+from project.TRANSLOC import main
 
 app = create_app()
 app.app_context().push()
 
 while 1:
     print ([(User.query.get(e.user_id).email, e.stop) for e in Event.query.all()])
-    print ([e.user_id for e in Event.query.filter(Event.time - 0 < 10).all()])
-    time.sleep(1)
+    a = ([e.user_id for e in Event.query.filter(Event.time - 0 < 10).all()])
+    print (User.query.get(e.user_id).email, e.stop, e.bus) #phone number, and two bus stops
+    main(User.query.get(e.user_id).email, e.stop, e.bus)
+    time.sleep(30)
