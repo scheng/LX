888fee70bab53432f6e304cfa3fc22495efa4964
scheng0 <scheng2019@gmail.com>
2019-10-20 03:12:44 -0400

    event tracker

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 import time
+
+from project import create_app
+from project.models import Event
+from project.models import User
+
+app = create_app()
+app.app_context().push()
+
 while 1:
-    print ("hello world")
+    print (Event.query.all())
     time.sleep(1)
