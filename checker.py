476b79c14ad2f0891e97a62390aa2a42660faab4
scheng0 <scheng2019@gmail.com>
2019-10-20 03:48:13 -0400

    user

--- checker.py
+++ checker.py
@@ -8,6 +8,6 @@ app = create_app()
 app.app_context().push()
 
 while 1:
-    print ([e.user_id for e in Event.query.all()])
+    print ([(User.query.get(e.user_id).email, e.stop) for e in Event.query.all()])
     print ([e.user_id for e in Event.query.filter(Event.time - 0 < 10).all()])
     time.sleep(1)
